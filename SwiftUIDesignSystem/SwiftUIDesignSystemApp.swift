8e686e9561ef67d6d8ee9a4ddcfd06e4ed794d9f
Jake Strickler <jakestrickler@icloud.com>
2026-02-24 14:32:15 -0500

    Initial Commit

---
+++ SwiftUIDesignSystem/SwiftUIDesignSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignSystemApp.swift
+//  SwiftUIDesignSystem
+//
+//  Created by Jake on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
